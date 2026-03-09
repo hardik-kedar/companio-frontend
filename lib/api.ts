514e5ffd2cddcf36a5e4edc1c5a1da92ebbbf2cb
hardik-kedar <hardik.kedar2004@gmail.com>
2026-03-09 22:13:19 +0530

    fix api url

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,28 @@
+
+// const API_URL = "https://companio-backend-production.up.railway.app";
+//   console.log("API_URL:", API_URL);
+
+// export const apiFetch = async (
+//   endpoint: string,
+//   options: RequestInit = {}
+// ) => {
+
+//   const url = `${API_URL}${endpoint}`;
+
+//   const response = await fetch(url, {
+//     credentials: "include",
+//     headers: {
+//       "Content-Type": "application/json",
+//       ...(options.headers || {})
+//     },
+//     ...options
+//   });
+
+//   return response;
+// };
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "http://localhost:5000";
+  "https://companio-backend-production.up.railway.app";
 
 export const apiFetch = async (
   endpoint: string,
