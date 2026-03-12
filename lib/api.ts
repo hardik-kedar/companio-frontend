8deaf1e1230bc42568cfd7bfcebea54d5d37b393
hardik-kedar <hardik.kedar2004@gmail.com>
2026-03-12 14:15:38 +0530

    use api.trycompanio.in backend

--- lib/api.ts
+++ lib/api.ts
@@ -1,25 +1,4 @@
 
-// const API_URL = "https://companio-backend-production.up.railway.app";
-//   console.log("API_URL:", API_URL);
-
-// export const apiFetch = async (
-//   endpoint: string,
-//   options: RequestInit = {}
-// ) => {
-
-//   const url = `${API_URL}${endpoint}`;
-
-//   const response = await fetch(url, {
-//     credentials: "include",
-//     headers: {
-//       "Content-Type": "application/json",
-//       ...(options.headers || {})
-//     },
-//     ...options
-//   });
-
-//   return response;
-// };
 const API_URL = "https://api.trycompanio.in";
 
 export const apiFetch = async (
