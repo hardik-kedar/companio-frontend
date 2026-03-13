16682953e0a79c26deb068c21d74984d65263c45
hardik-kedar <hardik.kedar2004@gmail.com>
2026-03-13 14:23:11 +0530

    fixed favicon path

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,26 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
-export const metadata: Metadata = {
-  title: "Companio — Feel Connected",
-  description:
-    "Companio helps people feel connected, understood, and supported.",
+// export const metadata = {
+//   title: "Companio",
+//   icons: {
+//     icon: "/logo/companio-logo.png",
+//   },
+// };
+
+// export const metadata: Metadata = {
+//   title: "Companio — Feel Connected",
+//   description:
+//     "Companio helps people feel connected, understood, and supported.",
+// };
+
+
+export const metadata = {
+  title: "Companio – Feel Connected",
+  description: "Companio helps people feel connected, understood and supported.",
+  icons: {
+    icon: "\logo\companio-logo.jpeg",
+  },
 };
 
 export default function RootLayout({
